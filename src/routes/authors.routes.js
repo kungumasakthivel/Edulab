bc48b0c25946cea22b7056c550a869675dd80dd3
kungumasakthivel <kungumasakthivel.k@gmail.com>
2024-08-26 09:32:57 +0530

    author edit api done

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -82,6 +82,59 @@ authorsRoute.post('/authors', (req, res) => {
     )
 });
 
+// edit author info with admin passcode
+authorsRoute.patch('/authors', (req, res) => {
+    const {id, passcode, name, bio} = req.body;
+
+    if(!id || !passcode || !name || !bio) {
+        return res.status(400).json({
+            message: "Bad request: Invalid input, information are missing",
+            status: 0
+        });
+    }
+
+    if(passcode !== process.env.ADMIN_PASSWORD) {
+        return res.status(404).json({
+            message: "Unauthorized access to edit author",
+            status: 0
+        });
+    }
+
+    db.get(
+        'SELECT COUNT(*) FROM authors WHERE id = ?', [id], (err, row) => {
+            if(err) {
+                return res.status(400).json({
+                    message: "No author with id: " + id,
+                    status: 0
+                });
+            }
+            if(row['COUNT(*)'] > 0) {
+                db.run(
+                    'UPDATE authors SET name = ?, bio = ? WHERE id = ?',
+                    [name, bio, id],
+                    function(err) {
+                        if(err) {
+                            return res.status(400).json({
+                                message: "error in updating author",
+                                status: 0
+                            });
+                        }
+                        res.status(200).json({
+                            message: "Author updated successfully",
+                            status: 1
+                        });
+                    }
+                );
+            } else {
+                return res.status(404).json({
+                    message: "author not found",
+                    status: 0
+                });
+            }
+        }
+    )
+});
+
 // delete author with admin passcode
 authorsRoute.delete('/authors', (req, res) => {
     const {id, passcode} = req.body;
